57ce1fa5010c65f1ad59eac2e341c00bbbf1072c
Ved Patel <158240030+ved-patel226@users.noreply.github.com>
2024-05-01 18:50:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ import time
 app = Flask(__name__)
 
 # Load posts from the provided API endpoint
-posts = requests.get("https://api.npoint.io/a3490619a628b4e0940a").json()
+posts = requests.get("https://api.npoint.io/783f6c134b3269a4e293").json()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Get the candy name from the request payload
-        candy_name = request.json.get('candy_name')
-        price_real = request.json.get('price_real')
-        
+        candy_name = request.form['candy_name']
+        person_name = request.form['person_name']
+        candy_price = request.form['candy_price']
         # Handle the form submission here
         # Replace 'WEBHOOK_URL_HERE' with your actual Discord webhook URL
         webhook_url = 'https://discord.com/api/webhooks/1232823457406910485/5-j04hyT8HjrYnQaQm0FhY-3dV67l3hUEjWxmdT5mPt9wkbebA2H0YJVblClBZ-vo0J1'
-        content = f'Order Submitted for {candy_name} with price (teerth i quit price)!'
+        content = f'Order Submitted for {candy_name} by {person_name} they will pay you {candy_price} YAYYY MONNEY!'
         webhook = DiscordWebhook(url=webhook_url, content=content)
         response = webhook.execute()
-        return ''  # Return an empty response to the AJAX request
+        return render_template('index.html', all_posts=posts, success=True)
     else:
-        return render_template('index.html', all_posts=posts)
+        return render_template('index.html', all_posts=posts, success=False)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
